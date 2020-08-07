c618fb9d818e2b054af7e528132c29ed5cab86eb
Vasko Popov <57752249+vaskopopov@users.noreply.github.com>
2020-08-07 17:02:58 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 
-		$(document).on('keyup','#prostorija',function(){
-			let prostorija=parseInt($('#prostorija').val());
-			$('#tuvSelect').val('');
-			$('.optimalnaKolicina').html("");
-			$('.potrebnaRadijacija').html("");
-			let info="<p>За дезинфекција на простор од <span class='prostor'></span>m<sup>2</sup> потребни се <span class='radijacija'></span>W UV-C зрачење</p>";
-			radijacijaProstor=parseFloat(prostorija)*0.35*1.25/0.8;
-			
-			$('.potrebnaRadijacija').append(info);
-			$('.prostor').html(prostorija);
-			$('.radijacija').html(radijacijaProstor.toFixed(4));
-			// $('.potrebnaRadijacija').html(radijacijaProstor);
-		});
+	$(document).on('keyup','#prostorija',function(){
+		let prostorija=parseInt($('#prostorija').val());
+		$('#tuvSelect').val('');
+		$('.optimalnaKolicina').html("");
+		$('.potrebnaRadijacija').html("");
+		let info="<p>За дезинфекција на простор од <span class='prostor'></span>m<sup>2</sup> потребни се <span class='radijacija'></span>W UV-C зрачење</p>";
+		radijacijaProstor=parseFloat(prostorija)*0.35*1.25/0.8;
+
+		$('.potrebnaRadijacija').append(info);
+		$('.prostor').html(prostorija);
+		$('.radijacija').html(radijacijaProstor.toFixed(4));
+		$('.potrebnaRadijacija').html(radijacijaProstor);
+	});
 // 	$(document).on('keyup','#prostorija',function(){
 // 	let prostorija=parseFloat($('#prostorija').val());
 // 		console.log(prostorija);
@@ -31,25 +31,25 @@ $(document).ready(function(){
 // 		console.log(data);
 		
 // 	 });
-	radijacijaProstor=parseFloat(prostorija)*0.35*1.25/0.8;
+// 	radijacijaProstor=parseFloat(prostorija)*0.35*1.25/0.8;
 	
-	$('.potrebnaRadijacija').append(info);
-	$('.prostor').html(prostorija);
-	$('.radijacija').html(radijacijaProstor.toFixed(4));
-	$('.potrebnaRadijacija').html(radijacijaProstor);
-});
+// 	$('.potrebnaRadijacija').append(info);
+// 	$('.prostor').html(prostorija);
+// 	$('.radijacija').html(radijacijaProstor.toFixed(4));
+// 	$('.potrebnaRadijacija').html(radijacijaProstor);
+// 	});
 
-		$('#tuvSelect').on('change',function(){
-			let optimalnaQtyHtml=$('.optimalnaKolicina').html("");
-			let radijacija=$(this).val();
-			
-			let html = '<p>Одбраната сијалица има <b>'+ radijacija+'W</b> UV-C радијација.</p><br>';
-			// html+='<p>За дезинфекција на простор од ' + $('#prostorija').val() + 'm<sup>2</sup> потребни се  ' +radijacijaProstor.toFixed(4) + 'W UV-C зрачење</p>';
-			optimalnaQtyHtml.append(html);
-			let optQty=Math.ceil(radijacijaProstor/radijacija);
-			html='<p>Според одбраната сијалица, оптимална потребна количина би била <b>'+optQty+'</b> ('+(radijacija*optQty).toFixed(2)+'W UV-C)<p>';
-			optimalnaQtyHtml.append(html);
-			
-			
-		});
+	$('#tuvSelect').on('change',function(){
+		let optimalnaQtyHtml=$('.optimalnaKolicina').html("");
+		let radijacija=$(this).val();
+
+		let html = '<p>Одбраната сијалица има <b>'+ radijacija+'W</b> UV-C радијација.</p><br>';
+		// html+='<p>За дезинфекција на простор од ' + $('#prostorija').val() + 'm<sup>2</sup> потребни се  ' +radijacijaProstor.toFixed(4) + 'W UV-C зрачење</p>';
+		optimalnaQtyHtml.append(html);
+		let optQty=Math.ceil(radijacijaProstor/radijacija);
+		html='<p>Според одбраната сијалица, оптимална потребна количина би била <b>'+optQty+'</b> ('+(radijacija*optQty).toFixed(2)+'W UV-C)<p>';
+		optimalnaQtyHtml.append(html);
+
+
 	});
+});
