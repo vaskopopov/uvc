dd98b5c4f2008cfa6858800806142895b1127cdc
Vasko Popov <57752249+vaskopopov@users.noreply.github.com>
2020-10-15 17:57:42 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
 
-// 	($('#prostorija').val()=='' || $('#tuvSelect').val()=='') ? $('#infoReport').hide() : $('#infoReport').show()
+	let radijacijaProstor, prostorija, info, optimalnaQtyHtml, radijacija, html, optQty;
 	
-// 	$('#prostorija').on('keyup',function(){
-// 		$(this).val()=='' ? $('#infoReport').hide() : $('#infoReport').show(); 
-// 	});
 	
 	$(document).on('keyup','#prostorija',function(){
-		let prostorija=parseInt($('#prostorija').val());
+		prostorija=parseInt($('#prostorija').val());
+		
 		$('#tuvSelect').val('');
 		$('#infoReport').hide();
 		$('.optimalnaKolicina').html("");
 		$('.potrebnaRadijacija').html("");
-		let info="<p>За дезинфекција на простор од <span class='prostor'></span>m<sup>2</sup> потребни се <span class='radijacija'></span>W UV-C зрачење</p>";
-		let radijacijaProstor=parseFloat(prostorija)*0.35*1.25/0.8;
+		
+		info="<p>За дезинфекција на простор од <span class='prostor'></span>m<sup>2</sup> потребни се <span class='radijacija'></span>W UV-C зрачење</p>";
+		radijacijaProstor=parseFloat(prostorija)*0.35*1.25/0.8;
 
 		$('.potrebnaRadijacija').append(info);
 		$('.prostor').html(prostorija);
@@ -24,18 +23,20 @@ $(document).ready(function(){
 	})
 	
 	$('#tuvSelect').on('change',function(){
-		let optimalnaQtyHtml=$('.optimalnaKolicina').html("");
-		let radijacija=$(this).val();
+		optimalnaQtyHtml=$('.optimalnaKolicina').html("");
+		radijacija=$(this).val();
 		
 		if ($('#prostorija').val()==""){
 			alert('Немате внесено квадратура на просторија');
 			$('#tuvSelect').val('');
 			$('#prostorija').focus();
 		} else {
-			let html = '<p>Одбраната сијалица има <b>'+ radijacija+'W</b> UV-C радијација.</p><br>';
+			
+			html = '<p>Одбраната сијалица има <b>'+ radijacija+'W</b> UV-C радијација.</p><br>';
 			// html+='<p>За дезинфекција на простор од ' + $('#prostorija').val() + 'm<sup>2</sup> потребни се  ' +radijacijaProstor.toFixed(4) + 'W UV-C зрачење</p>';
 			optimalnaQtyHtml.append(html);
-			let optQty=Math.ceil(radijacijaProstor/radijacija);
+			
+			optQty=Math.ceil(radijacijaProstor/radijacija);
 			html='<p>Според одбраната сијалица, оптимална потребна количина би била <b>'+optQty+'</b> ('+(radijacija*optQty).toFixed(2)+'W UV-C)<p>';
 			optimalnaQtyHtml.append(html);
 		}
